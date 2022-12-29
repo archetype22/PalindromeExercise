62377f13310b7685a8e51d453a4857c01d775f2e
nathan stephens <nstephens1234567@gmail.com>
2022-12-29 14:08:48 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var formated = string.Concat(word.Where(x => !char.IsPunctuation(x) && !char.IsSeparator(x) && !char.IsSymbol(x))).ToLower();
+            return formated == string.Concat(formated.Reverse());
+
+            //string reversedWord = "";
+
+            //for (int i = word.Length - 1; i >= 0; i--) 
+            //{
+            //    reversedWord += word[i];
+            //}
+            //return (reversedWord.ToLower() == word.ToLower());            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,27 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("raCecar", true)] // ← you will need to put some test data and expected result here.  
+        [InlineData("woW", true)]
+        [InlineData("mOm", true)]
+        [InlineData("noOn", true)]
+        [InlineData("banana", false)]
 
+        // For example [InlineData(“racecar”, true)]
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            //Arrange
+
+            var instance = new WordSmith();
+
+            //Act
+
+            var actual = instance.IsAPalindrome(word);
+
+            //Assert
+
+            Assert.Equal(expected, actual);
         }
     }
 }
